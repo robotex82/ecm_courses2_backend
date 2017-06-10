c8c195b831b8ba68c29f84714cedea863681114e
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2017-06-11 00:38:58 +0200

    Move to .ruby-* files. Fix load scope for courses. Cleanup. Add :en translations.

--- app/controllers/ecm/courses/backend/course_categories_controller.rb
+++ app/controllers/ecm/courses/backend/course_categories_controller.rb
@@ -2,15 +2,13 @@ module Ecm
   module Courses
     module Backend
       class CourseCategoriesController < Itsf::Backend::Resource::BaseController
+        include Controller::FriendlyIdConcern
+
         def self.resource_class
           Ecm::Courses::CourseCategory
         end
 
         private
-
-        def load_resource
-          load_scope.friendly.find(params[:id])
-        end
 
         def permitted_params
           params.require(:course_category).permit(:parent_id, :locale, :name, :description)
--- app/controllers/ecm/courses/backend/courses_controller.rb
+++ app/controllers/ecm/courses/backend/courses_controller.rb
@@ -3,20 +3,13 @@ module Ecm
     module Backend
       class CoursesController < Itsf::Backend::Resource::BaseController
         include Controller::ActsAsListConcern
+        include Controller::FriendlyIdConcern
 
         def self.resource_class
           Ecm::Courses::Course
         end
 
         private
-
-        def load_scope
-          resource_class.friendly
-        end
-
-        # def load_resource
-        #   load_scope.friendly.find(params[:id])
-        # end
 
         def permitted_params
           params.require(:course).permit(:course_category_id, :locale, :name, :description)
